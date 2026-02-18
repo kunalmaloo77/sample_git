2d4796ce44cc64502fe20db7213a94f33af3f9dc
kunalmaloo <kunnalmaloo@gmail.com>
2026-02-18 11:47:19 +0530

    another main commit

--- jsplayground/index.js
+++ jsplayground/index.js
@@ -8,3 +8,7 @@ console.log(Object.entries(obj));
 for (const [key, value] of Object.entries(obj)) {
 	console.log(`${key}: ${value}`);
 }
+
+const arr2 = [{ name: "Kunal", age: 22 }, { sports: "cricket" }];
+const findObj = arr2.find((item) => item.name === "Kunal");
+console.log(findObj);
